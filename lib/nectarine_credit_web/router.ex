0393aac289f31a5a0ab5127829b829bfec614636
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-09-04 11:31:05 +0700

    customize prod.exs for interviewer test. I will deploy a quick server for it

--- lib/nectarine_credit_web/router.ex
+++ lib/nectarine_credit_web/router.ex
@@ -34,19 +34,20 @@ defmodule NectarineCreditWeb.Router do
   # end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:nectarine_credit, :dev_routes) do
+  # Due to test and simulator, I will remove it
+
     # If you want to use the LiveDashboard in production, you should put
     # it behind authentication and allow only admins to access it.
     # If your application does not have an admins-only section yet,
     # you can use Plug.BasicAuth to set up some basic authentication
     # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
+  import Phoenix.LiveDashboard.Router
 
-    scope "/dev" do
-      pipe_through :browser
+  scope "/dev" do
+    pipe_through :browser
 
-      live_dashboard "/dashboard", metrics: NectarineCreditWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
+    live_dashboard "/dashboard", metrics: NectarineCreditWeb.Telemetry
+    forward "/mailbox", Plug.Swoosh.MailboxPreview
   end
+
 end
